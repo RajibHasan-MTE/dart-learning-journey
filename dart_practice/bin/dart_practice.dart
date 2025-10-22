28504022d146d52f7b499c7da1a458ff9feb08f0
Rajib Hasan <rajibhasan.mte@mail.com>
2025-10-22 23:25:27 +0600

    last code unary operator

--- dart_practice/bin/dart_practice.dart
+++ dart_practice/bin/dart_practice.dart
@@ -45,11 +45,51 @@ void main(){
   */
 
   /******* 6: Dart Lists *******/
+  /*
   var List = [1, 2, 3, 4, 5];
   print(List);
   print(List[1]);
   var city = ['Dhaka', 'Rangpur', 'Khulna', 'Cumilla'];
   print(city);
   print(city[0]);
+  print(city[3]);
+   */
+
+  /******* 7: Dart Maps *******
+   * The maps type is used to sotre value in key-value paris. Each key is associated with it value.
+   * The key and value can be any type. In map, the key must be unique, but a value can occur multiple times.
+   * The map is defined by using carly braces({}), and comma seperates each pair.
+   */
+  /*
+  var student = {'Name' : 'Rajib', 'Age' : 23, 'Branch' : 'Mechatronics engineering'};
+  print(student['Name']);
+  print(student['Age']);
+  print(student['Branch']);
+
+  var person = {
+    'Name' : 'Rajb Hasan',
+    'Age' : 23,
+    'City' : 'Cumilla',
+    'Height' : '5 Feet 7 Inch'
+  };
+  print(person['Name']);
+  print(person['Age']);
+  print(person['City']);
+  print(person['Height']);
+   */
+  /******* 8: Dart Operators *******
+   * 1. Addition (+)
+   * 2. subtraction (-)
+   * 3. Divide (/)
+   * 4. Multiplication (*)
+   * 5. Modulus (%)
+   * 6. Division (~/): It divide the first operand by the second operand and returns integer quotient a/b will return 2;
+   * 7. Unary Minus (- expr): it is used with a single operand changes the sign of it >> -(a-b) will return -10;
+   * >> 1. ++(prefix): It increment the value of operand >> ++x;
+   * >> 2. ++(postfix): It returns the actual value of operand before increment >> x++;
+   * >> 3. --(prefix): Decrement the value of the operand >> --x;
+   * >> 4. --(postfix): It returns the actual value of Operand before Decrement >> x--;
+   */
+  
 
 }
