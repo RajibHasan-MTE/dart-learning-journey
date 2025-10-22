c5d87033224a752eb0209a538c3b8b322e8372ac
Rajib Hasan <rajibhasan.mte@mail.com>
2025-10-22 23:00:04 +0600

    added dart pracitce file

---
+++ dart_practice/bin/dart_practice.dart
@@ -0,0 +1,55 @@
+
+
+
+
+
+import 'dart:typed_data';
+
+void main(){
+
+  /******* 1: Dart beginning *******/
+    /*
+    print("Hello dart")
+    */
+
+  /******* 2: Dart Variable *******/
+    /*
+    var x = 10;
+    var y = 40;
+    var z = x+y;
+    print(z);
+    */
+
+  /******* 3: Dart Number *******/
+  /*
+  var intNumber = 10;
+  var doubleNumber = 10.10;
+  print(intNumber);
+  print(doubleNumber);
+  */
+
+  /******* 4: Dart string *******/
+  /*
+  var myStringSingle = "This is a single";
+  var myStringDouble = "This is a double";
+  print(myStringSingle);
+  print(myStringDouble);
+  */
+
+  /******* 5: Dart Boolean *******/
+  /*
+  var negative = false;
+  var positive = true;
+  print(negative);
+  print(positive);
+  */
+
+  /******* 6: Dart Lists *******/
+  var List = [1, 2, 3, 4, 5];
+  print(List);
+  print(List[1]);
+  var city = ['Dhaka', 'Rangpur', 'Khulna', 'Cumilla'];
+  print(city);
+  print(city[0]);
+
+}
